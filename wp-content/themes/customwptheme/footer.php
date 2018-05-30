98c6f633c0a39709aea2f07490d6d3f7cc0253e9
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-05-30 14:10:08 -0700

    Created the home page.

--- wp-content/themes/customwptheme/footer.php
+++ wp-content/themes/customwptheme/footer.php
@@ -10,13 +10,24 @@
  */
 
 ?>
+  <!-- SIGN UP -->
+  <section id="signup" data-type="background" data-speed="4">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-sm-6">
+          <h2>Are you ready to take your coding skills to the <strong>next level</strong>?</h2>
+          <p><a href="#" class="btn btn-lg btn-block btn-success">Yes, sign me up!</a></p>
+        </div>
+      </div>
+    </div>
+  </section>
 
 	<footer>
     <div class="container">
       <div class="row">
         <div class="col-sm-3">
           <p>
-            <a href="#"><img src="assets/img/logo.png" alt="Bootstrap To Wordpress"></a>
+            <a href="#"><img src="<?php bloginfo('template_directory')?>/assets/img/logo.png" alt="Bootstrap To Wordpress"></a>
           </p>
         </div>
         <div class="col-sm-6">
@@ -35,7 +46,46 @@
         </div>
       </div>
     </div>
-	</footer>
+  </footer>
+  
+  <!-- MODAL -->
+  <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="myModalLabel">
+            <i class="fas fa-envelope"></i> Subscribe To Our Mailing List</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non ab aperiam voluptatibus asperiores perspiciatis facilis
+            eum voluptates dolorum unde ducimus.</p>
+
+          <form>
+            <div class="form-group">
+              <label for="subscribe-name">Your Name</label>
+              <input type="text" class="form-control" id="subscribe-name" placeholder="Your Name">
+            </div>
+            <div class="form-group">
+              <label for="subscribe-email">Your Email</label>
+              <input type="email" class="form-control" id="subscribe-email" placeholder="Your Email">
+            </div>
+            <button type="submit" class="btn btn-danger text-center">Subscribe!</button>
+          </form>
+          <hr>
+          <p>
+            <small>By providing your email you consent to receiving occasional promotional emails &amp; newsletters. No Spam. Just
+              good stuff. We respect your privacy &amp; you may unsubscribe at any time.</small>
+          </p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
 	
 	<script src="<?php bloginfo('template_directory')?>/assets/js/jquery.js"></script>
   <script src="<?php bloginfo('template_directory')?>/assets/js/main.js"></script>
---
+++ wp-content/themes/customwptheme/page-home.php
@@ -0,0 +1,382 @@
+<?php
+/*
+  Template Name: Home Page
+*/
+
+get_header(); ?>
+
+<!-- HERO -->
+<section id="hero" data-type="background" data-speed="5">
+    <article>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-5">
+            <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/logo-badge.png" alt="BTW">
+          </div>
+          <div class="col-sm-7 hero-text">
+            <h1>Bootstrap to Wordpress</h1>
+            <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae deleniti error, veritatis ullam consectetur odio.</p>
+            <div id="price-timeline">
+              <div class="price active">
+                <h4>Pre-Launch Price
+                  <small>Ends soon!</small>
+                </h4>
+                <span>$149</span>
+              </div>
+              <div class="price">
+                <h4>Launch Price
+                  <small>Ends soon!</small>
+                </h4>
+                <span>$299</span>
+              </div>
+              <div class="price">
+                <h4>Final Price
+                  <small>Ends soon!</small>
+                </h4>
+                <span>$399</span>
+              </div>
+              <!-- price -->
+            </div>
+            <!-- price-timeline -->
+            <p>
+              <a href="#" class="btn btn-lg btn-danger">Enroll &raquo;</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </article>
+  </section>
+
+  <!-- OPT IN -->
+  <section id="optin">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-8">
+          <p class="lead">
+            <strong>Subscrible to our mailing list.</strong> We'll send something special as a thank you.
+          </p>
+        </div>
+        <div class="col-sm-4">
+          <button class="btn btn-success btn-lg btn-block" data-toggle="modal" data-target="#myModal">
+            Subscribe Now
+          </button>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- BOOST YOUR INCOME -->
+  <section id="boost-income">
+    <div class="container">
+      <div class="section-header">
+        <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-boost.png" alt="Chart">
+        <h2>How You Can Boost Your Income</h2>
+        <p class="lead">Whether you’re a freelance designer, entrepreneur, employee for a company, code hobbyist, or looking for a new career — this course gives you an immensely valuable skill that will enable you to either:</p>
+      </div>
+
+      <div class="row">
+        <div class="col-sm-6">
+          <h3>Make Money on The Side</h3>
+          <p>So you can save up for that Hawaiian vacation you’ve been wanting, help pay off your debt, your car, your mortgage,
+            or simply just to have bonus cash laying around.</p>
+        </div>
+        <div class="col-sm-6">
+          <h3>Create a Full-Time Income</h3>
+          <p>WordPress developers have options. Many developers make a generous living off of creating custom WordPress themes
+            and selling them on websites like ThemeForest. Freelance designers and developers can also take on WordPress
+            projects and make an extra $1,000 - $5,000+ per month.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- WHO BENEFITS -->
+  <section id="who-benefits">
+    <div class="container">
+      <div class="section-header">
+        <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-pad.png" alt="Pad and pencil">
+        <h2>Who Should Take This Course?</h2>
+      </div>
+
+      <div class="row justify-content-center">
+        <div class="col-sm-8">
+          <h3>Graphic & Web Designers</h3>
+          <p>Graphic designers are extremely talented, but ask them to code their designs and they’ll freeze up! This leaves
+            them with no other choice but to hire a web developer. Any professional graphic designers knows web developers
+            can be expensive.</p>
+          <p> If you’re a designer, learning to code your own WordPress websites can change your business entirely! Now, not
+            only are you a great designer, but you’re a skillful developer, too! This puts you in a position to
+            <strong>make an extra $1,000 – $5,000 per project.</strong>
+          </p>
+
+          <h3>Entrepreneurs</h3>
+          <p>Entrepreneurs have big dreams, and in many cases, shoestring budgets. In order to survive in the cut-throat world
+            of the Startup company, it’s a necessity to have a world-class website. However, world-class websites come with
+            a large price tag.</p>
+          <p>If you can learn how to build a high-quality startup website by yourself, then you’ve just saved yourself a lot
+            of cash,
+            <strong>tens of thousands of dollars in many cases.</strong>
+          </p>
+
+          <h3>Employees</h3>
+          <p>Any company knows the education & training of their employees is key to a thriving team.</p>
+          <p>Depending on the type of company you work for, if you understand how to code, and can develop CMS driven websites,
+            that gives you
+            <strong>negotiating power for a better position, or a higher salary.</strong>
+          </p>
+
+          <h3>Code Hobbyists</h3>
+          <p>It’s fun to learn challenging new skills. Code hobbyists can add dynamic websites to their arsenal of tools to
+            play with — you can even sell
+            <strong>WordPress themes and plugins for cash!</strong> The possibilities are truly endless.</p>
+
+          <h3>People Looking for a New Career</h3>
+          <p>Are you out of work? Looking for a more rewarding job? Desire a career that can allow you to work almost anywhere
+            in the world? Becoming a Web Developer might be the answer for you.</p>
+          <p>
+            <strong>Web developers are paid well, anywhere from $33,000 to more than $105,000 per year.</strong> They get to work
+            at amazing companies that are changing the world, or they enjoy the ability to start their own companies, become
+            location-independent and work from home, from coffee shops, in an airplane, on the beach, or wherever they want!</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- COURSE FEATURES -->
+  <section id="course-features">
+    <div class="container">
+      <div class="section-header">
+        <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-rocket.png" alt="Rocket">
+        <h2>Course Features</h2>
+      </div>
+
+      <div class="row">
+        <div class="col-sm-2">
+          <i class="ci ci-computer"></i>
+          <h4>Lifetime access to 80+ lectures</h4>
+        </div>
+        <div class="col-sm-2">
+          <i class="ci ci-watch"></i>
+          <h4>10+ hours of HD video content</h4>
+        </div>
+        <div class="col-sm-2">
+          <i class="ci ci-calendar"></i>
+          <h4>30-day money back guarantee</h4>
+        </div>
+        <div class="col-sm-2">
+          <i class="ci ci-community"></i>
+          <h4>Access to a community of like-minded students</h4>
+        </div>
+        <div class="col-sm-2">
+          <i class="ci ci-instructor"></i>
+          <h4>Direct access to the instructor</h4>
+        </div>
+        <div class="col-sm-2">
+          <i class="ci ci-device"></i>
+          <h4>Accessible content on your mobile devices</h4>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- PROJECT FEATURES -->
+  <section id="project-features">
+    <div class="container">
+      <h2>Final Project Features</h2>
+      <p class="lead">Throughout this entire course, you work towards building an incredibly beautiful website. Want to see the
+        <strong>website you</strong>
+        are going to build?
+        <em>You're looking at it!</em> The website you're using right now is the website you will have built entirely by yourself,
+        by the end of this course.</p>
+      <div class="row">
+        <div class="col-sm-4">
+          <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-design.png" alt="Design">
+          <h3>Sexy & Modern Design</h4>
+            <p>You get to work with a modern, professional quality design &amp; layout.</p>
+        </div>
+        <div class="col-sm-4">
+          <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-code.png" alt="Code">
+          <h3>Quality HTML5 & CSS3</h4>
+            <p>You'll learn how hand-craft a stunning website with valid, semantic and beautiful HTML5 & CSS3.</p>
+        </div>
+        <div class="col-sm-4">
+          <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-cms.png" alt="CMS">
+          <h3>Easy-to-use CMS</h3>
+          <p>Allow your clients to easily update their websites by converting your static websites to dynamic websites, using
+            WordPress.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- VIDEO FEATURETTE -->
+  <section id="featurette">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-sm-8">
+          <h2>Watch The Course Introduction</h2>
+          <iframe width="100%" height="415" src="https://www.youtube.com/embed/q-mJJsnOHew" frameborder="0" allow="autoplay; encrypted-media"
+            allowfullscreen></iframe>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- INSTRUCTOR -->
+  <section id="instructor">
+    <div class="container">
+      <!-- start container -->
+      <div class="row">
+        <!-- start first row -->
+        <div class="col-sm-8 col-md-6">
+          <!-- start first column -->
+          <div class="row">
+            <!-- start first nested row -->
+            <div class="col-lg-8">
+              <h2>Your Instructor
+                <small>Brad Hussey</small>
+              </h2>
+            </div>
+            <div class="col-lg-4">
+              <a href="#" class="badge social twitter">
+                <i class="fab fa-twitter"></i>
+              </a>
+              <a href="#" class="badge social facebook">
+                <i class="fab fa-facebook"></i>
+              </a>
+              <a href="#" class="badge social gplus">
+                <i class="fab fa-google-plus"></i>
+              </a>
+            </div>
+          </div>
+          <!-- end first nested row -->
+
+          <p class="lead">A highly skilled professional, Brad Hussey is a passionate and experienced web designer, developer, blogger and
+            digital entrepreneur.</p>
+          <p class="lead">Hailing from North Of The Wall (Yellowknife, Canada), Brad made the trek to the Wet Coast (Vancouver, Canada) to
+            educate and equip himself with the necessary skills to become a spearhead in his trade of solving problems on
+            the web, crafting design solutions, and speaking in code.</p>
+          <p class="lead">Brad's determination and love for what he does has landed him in some pretty interesting places with some neat
+            people. He's had the privilege of working with, and providing solutions for, numerous businesses, big & small,
+            across the Americas.</p>
+          <p class="lead">Brad builds custom websites, and provides design solutions for a wide-array of clientele at his company, Brightside
+            Studios. He regularly blogs about passive income, living your life to the fullest, and provides premium quality
+            web design tutorials and courses for tens of thousands of amazing people desiring to master their craft.</p>
+
+          <hr>
+
+          <h3>The Numbers
+            <small>They Don't Lie</small>
+          </h3>
+
+          <div class="row">
+            <!-- start second nested row -->
+            <div class="col-4">
+              <div class="num">
+                <div class="num-content">
+                  41,000+
+                  <span>students</span>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-4">
+              <div class="num">
+                <div class="num-content">
+                  568
+                  <span>reviews</span>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-4">
+              <div class="num">
+                <div class="num-content">
+                  8
+                  <span>courses</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- end second nested row -->
+
+        </div>
+        <!-- end first column -->
+      </div>
+      <!-- end first row -->
+    </div>
+    <!-- end container -->
+  </section>
+
+  <!-- TESTIMONIALS -->
+  <section id="testimonials">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-sm-8">
+          <h2>What People Are Saying About Brad</h2>
+          <div class="row testimonial">
+            <div class="col-sm-4">
+              <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/brennan.jpg" alt="Brennan">
+            </div>
+            <div class="col-sm-8">
+              <blockquote>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia in a voluptates iure optio nihil, consequatur ex incidunt
+                dolores aperiam corrupti possimus hic earum nisi corporis veniam! Blanditiis corrupti, tempora impedit mollitia
+                ratione assumenda at error recusandae ipsa est, animi, a vero nulla dolorem deleniti inventore! Corporis
+                non tempore reprehenderit?
+                <cite>&mdash; Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae, quasi.</cite>
+              </blockquote>
+            </div>
+          </div>
+
+          <div class="row testimonial">
+            <div class="col-sm-4">
+              <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/ben.png" alt="Ben">
+            </div>
+            <div class="col-sm-8">
+              <blockquote>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia in a vmus hic earum nisi corporis veniam! Blanditiis corrupti, tempora impedit mollitia
+                ratione assumenda at error recusandae ipsa est, animi, a vero nulla dolorem deleniti inventore! Corporis
+                non tempore reprehenderit?
+                <cite>&mdash; Lorem ipsum dolor, amet consectetur adipisicing elit. Repudiandae, quasi.</cite>
+              </blockquote>
+            </div>
+          </div>
+
+          <div class="row testimonial">
+            <div class="col-sm-4">
+              <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/aj.png" alt="Aj">
+            </div>
+            <div class="col-sm-8">
+              <blockquote>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia in a voluptates iure optio nihil, consequatur ex incidunt
+                dolores aperiam coris corrupti, tempora impedit mollitia
+                ratione assumenda at error recusandae ipsa est, animi, a vero nulla dolorem deleniti inventore! Corporis
+                non tempore reprehenderit?
+                <cite>&mdash; Lorsit amet consectetur adipisicing elit. Repudiandae, quasi.</cite>
+              </blockquote>
+            </div>
+          </div>
+
+          <div class="row testimonial">
+            <div class="col-sm-4">
+              <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/ernest.png" alt="Ernest">
+            </div>
+            <div class="col-sm-8">
+              <blockquote>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia in a voluptates iure optio nihil, consequatur ex incidunt
+                dolores aperiam corrupti possimus hic earum nisi
+                ratione assumenda at error recusandae ipsa est, animi, a vero nulla dolorem deleniti inventore! Corporis
+                non tempore reprehenderit?
+                <cite>&mdash; Lorem ipsum dolor, sit amet adipisicing elit. Repudiandae, quasi.</cite>
+              </blockquote>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+<?php get_footer(); ?>

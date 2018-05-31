a049609cd6cbd114e21281ffe1934c9d1dd3ec81
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-05-31 16:31:07 -0700

    Customized the archive template.

--- wp-content/themes/customwptheme/archive.php
+++ wp-content/themes/customwptheme/archive.php
@@ -9,45 +9,50 @@
 
 get_header();
 ?>
+	
+<?php if ( have_posts() ) : ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
+		<section class="feature-image feature-image-default-alt" data-type="background" data-speed="2">
+			<h1 class="page-title">
 				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<small class="archive-description">', '</small>' );
 				?>
-			</header><!-- .page-header -->
+			</h1>
+		</section>
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+	<div class="container">
+		<div id="primary" class="row">
+			<main id="content" class="col-sm-8">
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+	<?php
+	/* Start the Loop */
+	while ( have_posts() ) :
+		the_post();
 
-			endwhile;
+		/*
+			* Include the Post-Type-specific template for the content.
+			* If you want to override this in a child theme, then include a file
+			* called content-___.php (where ___ is the Post Type name) and that will be used instead.
+			*/
+		get_template_part( 'template-parts/content', get_post_type() );
 
-			the_posts_navigation();
+	endwhile;
 
-		else :
+	the_posts_navigation();
 
-			get_template_part( 'template-parts/content', 'none' );
+else :
 
-		endif;
-		?>
+	get_template_part( 'template-parts/content', 'none' );
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+endif;
+?>
+			</main>
+			<aside class="col sm-8">
+				<?php get_sidebar(); ?>
+			</aside>
+		</div>
+	</div>
 
 <?php
-get_sidebar();
 get_footer();
